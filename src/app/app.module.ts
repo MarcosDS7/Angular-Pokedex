b7dbe4da340b8a96e5cb39c4ae8a537780badf08
Marcos Vinícius Rodrigues de Souza <marcossouza.0ds@gmail.com>
2022-01-04 11:31:37 -0300

    Style pokemon-card flex-wrap

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -4,11 +4,13 @@ import { BrowserModule } from '@angular/platform-browser';
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { PokemonListComponent } from './pokemon-list/pokemon-list.component';
+import { PokemonCardComponent } from './pokemon-card/pokemon-card.component';
 
 @NgModule({
   declarations: [
     AppComponent,
-    PokemonListComponent
+    PokemonListComponent,
+    PokemonCardComponent
   ],
   imports: [
     BrowserModule,
